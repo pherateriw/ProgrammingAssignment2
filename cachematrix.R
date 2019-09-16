2de0a901a1739679be5e017289ddb97cea04320c
Janette Rounds <janette.rounds@gmail.com>
2019-09-16 15:02:47 -0600

    Added functions and tests

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    x_inv <- NULL
+    set <- function(y){
+        x <<- y
+        x_inv <<- NULL
+    }
+    get <- function() x
+    set_inverse <- function(inverted) x_inv <<- inverted
+    get_inverse <- function() x_inv
+    list(set = set, get = get, 
+         set_inverse = set_inverse, get_inverse = get_inverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    x_inv <- x$get_inverse()
+    if(!is.null(x_inv)){
+        message("getting cached inverse")
+        return(x_inv)
+    }
+    data <- x$get()
+    x_inv <- solve(data)
+    x$set_inverse(x_inv)
+    x_inv
 }
---
+++ cachematrixTest.R
@@ -0,0 +1,37 @@
+## functions and test cases for cachematrix.R
+
+## Function for generating a random square invertible matrix
+generateTest <- function(dimensions = 3, max_value = 100, retry = 0){
+    m <- matrix(sample.int(max_value, size=dimensions*dimensions, replace = TRUE), 
+           nrow = dimensions, ncol = dimensions)
+    det_m <- det(m)
+    if(det_m != 0){
+        return(m)
+    } else if(det_m == 0 & retry < 10){
+        retry <- retry + 1
+        warning('Attempt ', retry, ' to create invertible matrix failed. Retrying')
+        return(generateTest(dimensions = dimensions, 
+                            max_value = max_value, retry = retry))
+    } else {
+        warning('Not able to generate invertible matrix in 10 retries')
+        return(NA)
+    }
+}
+
+## Specific Test cases
+A3x3 = matrix(c(3,0,2,2,0,-2,0,1,1), nrow = 3, ncol = 3)
+A4x4 = matrix(c(1,0,0,1,0,2,1,2,2,1,0,1,2,0,1,4), nrow = 4, ncol = 4)
+
+#perform matrix muultiplication, round to precision, 
+#and check that the result is the identity matrix
+assertIsInverse <- function(m,i,prec = 0){
+    ident <- m %*% i
+    ident <- round(ident, prec)
+    for(row in 1:nrow(ident)){
+        if(ident[row,row] != 1 | sum(ident[row,]) != 1){
+            warning('This matrix is not the inverse!')
+            return(FALSE)
+        }
+    }
+    return(TRUE)
+}
